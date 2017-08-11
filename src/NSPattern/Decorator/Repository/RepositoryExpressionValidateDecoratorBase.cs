f58c47aed496f313e7864e9c34ae09ba78ca70a7
Mukmyash <mukminovartur@gmail.com>
2017-08-11 22:26:52 +0500

    Добавил Декоратор репозитолрия для Expression

---
+++ src/NSPattern/Decorator/Repository/RepositoryExpressionValidateDecoratorBase.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using NSPattern.Repository;
+using NSPattern.Specification;
+using NSPattern.Specification.Expression;
+
+namespace NSPattern.Decorator.Repository
+{
+    public abstract class RepositoryExpressionValidateDecoratorBase<TRepository, TEntity>
+    : DecoratorBase<TRepository>, IRepositoryExpression<TEntity>
+    where TRepository : IDecorateable, IRepositoryExpression<TEntity>
+    {
+        public virtual void Create_Before(TEntity entity) { }
+        public virtual void Create(TEntity entity)
+        {
+            Create_Before(entity);
+            Component.Create(entity);
+            Create_After(entity);
+        }
+        public virtual void Create_After(TEntity entity) { }
+
+
+        public virtual void GetById_Before(long id) { }
+        public virtual TEntity GetById(long id)
+        {
+            GetById_Before(id);
+            var result = Component.GetById(id);
+            GetById_After(id, result);
+            return result;
+        }
+        public virtual void GetById_After(long id, TEntity result) { }
+
+        public virtual void GetList_Before(ISpecificationExpression<TEntity> specification, int offset, int count) { }
+        public virtual IList<TEntity> GetList(ISpecificationExpression<TEntity> specification, int offset, int count)
+        {
+            GetList_Before(specification, offset, count);
+            var result = Component.GetList(specification, offset, count);
+            GetList_After(specification, offset, count, result);
+            return result;
+        }
+        public virtual void GetList_After(ISpecificationExpression<TEntity> specification, int offset, int count, IList<TEntity> result) { }
+
+
+        public virtual void Update_Before(TEntity entity) { }
+        public virtual void Update(TEntity entity)
+        {
+            Update_Before(entity);
+            Component.Update(entity);
+            Update_After(entity);
+        }
+        public virtual void Update_After(TEntity entity) { }
+
+    }
+}
